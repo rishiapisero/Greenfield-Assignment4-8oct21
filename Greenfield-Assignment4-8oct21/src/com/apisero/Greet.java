6beb2305a4ecda6218227db5498df5346155f484
ASUS <ASUS@LAPTOP-PAMO61Q5>
2021-10-08 18:24:37 +0530

    Commit by vishal

--- Greenfield-Assignment4-8oct21/src/com/apisero/Greet.java
+++ Greenfield-Assignment4-8oct21/src/com/apisero/Greet.java
@@ -3,8 +3,9 @@ package com.apisero;
 public class Greet {
 	
 	public static void main(String args[]) {
-		System.out.println("Changes by Rishi");
+		System.out.println("Changes by Rishi Soni");
 		System.out.println("Changes by Omkar");
 		System.out.println("Changes by Mayuri");
+		System.out.println("Changes by Vishal");
 	}
 }
